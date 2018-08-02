def624f01ced923fad8f2cbcadebac43c8e4b737
Naoya SATO <stny@colourful.jp>
2018-08-03 04:53:54 +0900

    Use `html-webpack-plugin`

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -19,4 +20,5 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
 };
